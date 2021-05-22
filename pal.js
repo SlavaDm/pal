a48d079979d22d2c44ac7b06f70382c4defc75c1
SlavaDm <65130162+SlavaDm@users.noreply.github.com>
2021-05-22 21:47:41 +0700

    Update pal.js

--- pal.js
+++ pal.js
@@ -1,26 +1,30 @@
-function pal(str){
-  const arr = str.split('').sort((a, b) => (a > b ? 1 : -1))
+function pal(str) {
+	const arr = str.split('').sort((a, b) => (a > b ? 1 : -1))
 
-  for(let i = 0; i < (arr.length - 1); i++){
-    if(arr[i] === arr[i+1]){
-      delete arr[i]
-      delete arr[i + 1]
-    }
-  }
+	for (let i = 0; i < (arr.length - 1); i++) {
+		if (arr[i] === undefined) {
+			continue
+		}
+		
+		if (arr[i] === arr[i + 1]) {
+			delete arr[i]
+			delete arr[i + 1]
+		}
+	}
 
-  const filteredArray = arr.filter(el=> el !== undefined)
+	const filteredArray = arr.filter(el => el !== undefined)
 
-  if(arr.length % 2 === 0){
-    if(filteredArray.length === 0){
-      return true
-    }
-  } else {
-    if(filteredArray.length === 1){
-      return true
-    }
-  }
+	if (arr.length % 2 === 0) {
+		if (filteredArray.length === 0) {
+			return true
+		}
+	} else {
+		if (filteredArray.length === 1) {
+			return true
+		}
+	}
 
-  return false
+	return false
 }
 
 console.log(pal('aacbb'))
