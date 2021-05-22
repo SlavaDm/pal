b37609553de958d9df3feba510703b3db14a5925
SlavaDm <65130162+SlavaDm@users.noreply.github.com>
2021-05-22 14:44:29 +0700

    Create pal.js

---
+++ pal.js
@@ -0,0 +1,26 @@
+function pal(str){
+  const arr = str.split('').sort((a, b) => (a > b ? 1 : -1))
+
+  for(let i = 0; i < (arr.length - 1); i++){
+    if(arr[i] === arr[i+1]){
+      delete arr[i]
+      delete arr[i + 1]
+    }
+  }
+
+  const filteredArray = arr.filter(el=> el !== undefined)
+
+  if(arr.length % 2 === 0){
+    if(filteredArray.length === 0){
+      return true
+    }
+  } else {
+    if(filteredArray.length === 1){
+      return true
+    }
+  }
+
+  return false
+}
+
+console.log(pal('aacbb'))
